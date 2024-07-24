0f2d100c6cba9e1c090d291e0a008c2dc154d31d
myuan <zhengmy@ion.ac.cn>
2024-07-24 23:35:41 +0800

    对不平衡数据做重要性采样, 为损失添加重要性权重

--- train.py
+++ train.py
@@ -1,31 +1,33 @@
 import json
 import os
+from collections import Counter
 from datetime import datetime
 
+import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 import torch.optim as optim
 from PIL import Image
 from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import train_test_split
-from torch.utils.data import DataLoader, Dataset
+from torch.utils.data import DataLoader, Dataset, WeightedRandomSampler
 from torch.utils.tensorboard import SummaryWriter
 from torchvision import models, transforms
-import cv2
+from pathlib import Path
 
 # 定义数据集类
 class ImageDataset(Dataset):
-    def __init__(self, image_folder, labels_file, transform=None):
+    label_types = ["普通人物", "动作人物", "选择卡片", "其他"]
+    
+    def __init__(self, image_folder: str, labels: dict[str, str], transform=None, included_img_names: set[str]=set()):
         self.transform = transform
-        self.label_types = ["普通人物", "动作人物", "选择卡片", "其他"]
         self.label_to_idx = {label: idx for idx, label in enumerate(self.label_types)}
 
-        # 加载标签
-        with open(labels_file, 'r') as f:
-            self.labels = json.load(f)
+        self.labels = labels
 
         # 初始化图像和标签列表
         self.images = []
@@ -33,6 +35,9 @@ class ImageDataset(Dataset):
 
         # 一次性加载所有图像到内存
         for img_name, label in self.labels.items():
+            if included_img_names and img_name not in included_img_names:
+                continue
+
             img_path = os.path.join(image_folder, img_name)
             image = cv2.imread(img_path)
             self.images.append(image)
@@ -57,50 +62,69 @@ class ImageDataset(Dataset):
 train_transform = transforms.Compose([
     transforms.ToTensor(),
 
-    transforms.RandomResizedCrop(224),
-    transforms.RandomHorizontalFlip(),
     transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),
     transforms.RandomRotation(10),
-    transforms.RandomAffine(0, translate=(0.1, 0.1)),
     transforms.RandomErasing(p=0.2),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 ])
 
 test_transform = transforms.Compose([
     transforms.ToTensor(),
-
-    transforms.Resize(256),
-    transforms.CenterCrop(224),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 ])
 
-# 加载数据集
-dataset = ImageDataset("./data/frames", "./labels.json", transform=None)
-train_indices, test_indices = train_test_split(range(len(dataset)), test_size=0.2, random_state=42)
+all_labels: dict[str, str] = json.load(open('./labels.json', 'r'))
+train_img_names, test_img_names = train_test_split(list(all_labels.keys()), test_size=0.2)
 
-train_dataset = ImageDataset("./data/frames", "./labels.json", transform=train_transform)
-test_dataset = ImageDataset("./data/frames", "./labels.json", transform=test_transform)
 
-train_sampler = torch.utils.data.SubsetRandomSampler(train_indices)
-test_sampler = torch.utils.data.SubsetRandomSampler(test_indices)
+class_counts = Counter(all_labels.values())
+num_samples = len(all_labels)
+class_weights = {cls: num_samples / count for cls, count in class_counts.items()}
+sample_weights = [class_weights[all_labels[n]] for n in train_img_names]
 
-train_loader = DataLoader(train_dataset, batch_size=32, sampler=train_sampler)
-test_loader = DataLoader(test_dataset, batch_size=32, sampler=test_sampler)
+sampler = WeightedRandomSampler(sample_weights, num_samples, replacement=True)
+
+train_dataset = ImageDataset("./data/frames", all_labels, transform=train_transform, included_img_names=set(train_img_names))
+test_dataset = ImageDataset("./data/frames", all_labels, transform=test_transform, included_img_names=set(test_img_names))
+
+
+train_loader = DataLoader(train_dataset, batch_size=256, sampler=sampler)
+test_loader = DataLoader(test_dataset, batch_size=256)
 
 # 定义模型
-model = models.resnet18(pretrained=True)
+model = models.resnet18(weights="IMAGENET1K_V1")
 num_ftrs = model.fc.in_features
 model.fc = nn.Linear(num_ftrs, 4)  # 4 classes
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model = model.to(device)
 
-# 定义损失函数和优化器
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.parameters(), lr=0.001)
+if os.path.exists('image_classifier.pth'):
+    model.load_state_dict(torch.load('image_classifier.pth'))
 
-# 设置 TensorBoard 2024-0618-2305
-writer = SummaryWriter(log_dir=f'runs/{datetime.now():%Y%m%d-%H%M}')
+# 定义损失函数和优化器
+criterion_weight = torch.Tensor([
+    class_weights[cls] 
+    for cls in ImageDataset.label_types
+]).to(device)
+print(f'{criterion_weight=} {class_weights=}')
+criterion = nn.CrossEntropyLoss(weight=criterion_weight)
+# class FocalLoss(nn.Module):
+#     def __init__(self, alpha=1, gamma=2):
+#         super(FocalLoss, self).__init__()
+#         self.alpha = alpha
+#         self.gamma = gamma
+    
+#     def forward(self, inputs, targets):
+#         ce_loss = F.cross_entropy(inputs, targets, reduction='none')
+#         pt = torch.exp(-ce_loss)
+#         focal_loss = self.alpha * (1-pt)**self.gamma * ce_loss
+#         return focal_loss.mean()
+
+# criterion = FocalLoss()
+
+optimizer = optim.SGD(model.parameters(), lr=0.0005)
+
+log_dir = Path(f'runs/{datetime.now():%Y%m%d-%H%M}')
+writer = SummaryWriter(log_dir=log_dir)
 
 # 训练函数
 def train(model, loader, criterion, optimizer, epoch):
@@ -123,10 +147,9 @@ def train(model, loader, criterion, optimizer, epoch):
         all_preds.extend(preds.cpu().numpy())
         all_labels.extend(labels.cpu().numpy())
 
-        if i % 10 == 9:
-            writer.add_scalar('training loss', running_loss / 10, epoch * len(loader) + i)
-            running_loss = 0.0
-
+        writer.add_scalar('training loss', running_loss, epoch * len(loader) + i)
+        running_loss = 0.0
+    # print(all_labels, all_preds)
     cm = confusion_matrix(all_labels, all_preds)
     fig, ax = plt.subplots(figsize=(10, 10))
     sns.heatmap(cm, annot=True, fmt='d', ax=ax)
@@ -164,13 +187,13 @@ def test(model, loader, criterion, epoch):
     return test_loss, accuracy
 
 # 训练循环
-num_epochs = 20
+num_epochs = 200
 for epoch in range(num_epochs):
     train(model, train_loader, criterion, optimizer, epoch)
     test_loss, accuracy = test(model, test_loader, criterion, epoch)
     print(f'Epoch {epoch+1}/{num_epochs}, Test Loss: {test_loss:.4f}, Accuracy: {accuracy:.2f}%')
 
+    torch.save(model.state_dict(), 'image_classifier.pth')
 writer.close()
 
 # 保存模型
-torch.save(model.state_dict(), 'image_classifier.pth')
