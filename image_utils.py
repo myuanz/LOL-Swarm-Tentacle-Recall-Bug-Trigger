e3c694d477443f05cb694688959d05bc8c319fe8
myuanz <provefars@gmail.com>
2024-07-29 21:26:01 +0800

    拆分两功能

--- image_utils.py
+++ image_utils.py
@@ -1,6 +1,31 @@
 import numpy as np
 from beartype import beartype
+from dataclasses import dataclass
 
+@beartype
+@dataclass
+class BBox:
+    x: int
+    y: int
+    w: int
+    h: int
+    
+def size_to_bbox[T: (int, float)](
+    img_w: int, img_h: int,
+    x: T, y: T, w: T, h: T
+) -> BBox:
+    types = [type(x), type(y), type(w), type(h)]
+    assert all(t is int for t in types) or all(t is float for t in types), "All types should be either int or float"
+    
+    if isinstance(x, float):
+        return BBox(
+            int(x * img_w),
+            int(y * img_h),
+            int(w * img_w),
+            int(h * img_w)
+        )
+    else:
+        return BBox(x, y, w, h) # type: ignore
 
 @beartype
 def crop_image[T: (int, float)](
@@ -10,16 +35,9 @@ def crop_image[T: (int, float)](
     copy: bool = False
 ) -> np.ndarray:
     img_h, img_w = image.shape[:2]
-    if isinstance(x, float):
-        _x = int(x * img_w)
-        _y = int(y * img_h)
-        _w = int(w * img_w)
-    else:
-        _x = x
-        _y = y
-        _w = w
+    box = size_to_bbox(img_w, img_h, x, y, w, w)
 
-    res = image[_y:_y+_w, _x:_x+_w]
+    res = image[box.y:box.y + box.h, box.x:box.x + box.w]
     if copy:
         return res.copy()
     return res
